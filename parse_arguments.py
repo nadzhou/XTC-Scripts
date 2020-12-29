6ce5c14d4a9e7fb5309f44e3596cb589a55e6e84
nadzhou <ahmaddnavid@gmail.com>
2020-12-29 07:19:57 +0500

    added 6 inputs

--- parse_arguments.py
+++ parse_arguments.py
@@ -8,6 +8,23 @@ def parse_arguments(parser=None):
     parser.add_argument("input_tpr", help="Input gromacs .tpr file")
     parser.add_argument("input_xtc", help="Input gromacs .xtc file")
 
+    parser.add_argument("--input_tpr2", default=None, help="Input gromacs .tpr file")
+    parser.add_argument("--input_tpr2", default=None, help="Input gromacs .tpr file")
+
+    parser.add_argument("--input_tpr3", default=None, help="Input gromacs .tpr file")
+    parser.add_argument("--input_xtc3", default=None, help="Input gromacs .tpr file")
+
+    parser.add_argument("--input_tpr4", default=None, help="Input gromacs .tpr file")
+    parser.add_argument("--input_xtc4", default=None, help="Input gromacs .tpr file")
+
+    parser.add_argument("--input_tpr5", default=None, help="Input gromacs .tpr file")
+    parser.add_argument("--input_xtc5", default=None, help="Input gromacs .tpr file")
+
+    parser.add_argument("--input_tpr6", default=None, help="Input gromacs .tpr file")
+    parser.add_argument("--input_xtc6", default=None, help="Input gromacs .tpr file")
+
+
+
     return parser.parse_args()
 
 
--- xtc_read.py
+++ xtc_read.py
@@ -20,15 +20,14 @@ def main():
     parser = pa.parse_arguments()
 
     print("loading universe...")
-    new_universe = mda.Universe(parser.input_tpr, parser.input_xtc)
 
-    if new_universe: 
-        analysis_list()
-        operation_input = int(input("Select which simulation analysis: "))
-        region_selection_check()
-        input_resid = input("Input selection parameters: ")
+    analysis_list()
+    operation_input = int(input("Select which simulation analysis: "))
+    region_selection_check()
+    input_resid = input("Input selection parameters: ")
 
-        analysis_routes(operation_input, new_universe, input_resid)
+    analysis_routes(operation_input, parser, input_resid)
+    plt.show()
 
 
 def region_selection_check(): 
@@ -47,8 +46,13 @@ def analysis_routes(analysis_choice, universe, atom_select):
     if analysis_choice == 1: 
         calc_pca(universe, atom_select)
 
+    elif analysis_choice == 2: 
+        calc_hbonds(universe)
+
     elif analysis_choice == 4: 
         calc_rmsf(universe, atom_select)
+
+
 
     
 def calc_rmsf(universe: mda.Universe, atom_select: mda.Universe):
@@ -102,37 +106,101 @@ def calc_distances_bn(ser_resids, tyr_resids) -> np.ndarray:
 
 
 
-def ser_tyr_residslc_hbonds(universe): 
+def calc_hbonds(universe): 
     """EXPERIMENTAL: 
             calculate hydrogens"""
 
-    nad_bonds = HBA(universe=universe)
-    nad_bonds.run(verbose=True)
+    hbonds = HBA(universe=universe)
+    hbonds.hydrogens_sel = hbonds.guess_hydrogens("protein")
+    hbonds.acceptors_sel = hbonds.guess_acceptors("protein")
+    hbonds.run(verbose=True)
+
+    tau_timeseries, timeseries = hbonds.lifetime()
+    print(tau_timeseries)
+    print(timeseries)
+
+    sns.lineplot(tau_timeseries, timeseries)
+    plt.show()
 
 
 
-def calc_pca(universe, atom_select):
+def calc_pca(parser, atom_select):
     import MDAnalysis.analysis.pca as pca
     import pandas as pd
     import seaborn as sns
+
+    universe_1 = mda.Universe(parser.input_tpr, parser.input_xtc)
+    universe_2 = mda.Universe(parser.input_tpr2, parser.input_xtc2)
+    universe_3 = mda.Universe(parser.input_tpr3, parser.input_xtc3)
+    universe_4 = mda.Universe(parser.input_tpr4, parser.input_xtc4)
+    universe_5 = mda.Universe(parser.input_tpr5, parser.input_xtc5)
+    universe_6 = mda.Universe(parser.input_tpr6, parser.input_xtc6)
+
+    pc = pca.PCA(universe_1, select=atom_select)
+    pc2 = pca.PCA(universe_2, select=atom_select)
+    pc3 = pca.PCA(universe_3, select=atom_select)
+    pc4 = pca.PCA(universe_4, select=atom_select)
+    pc5 = pca.PCA(universe_5, select=atom_select)
+    pc6 = pca.PCA(universe_6, select=atom_select)
     
-    pc = pca.PCA(universe, select=atom_select)
     pc.run(verbose=True)
+    pc2.run(verbose=True)
+    pc3.run(verbose=True)
+    pc4.run(verbose=True)
+    pc5.run(verbose=True)
+    pc6.run(verbose=True)
 
-    # print(tyr_pca.p_components.shape)
-    # plt.plot(tyr_pca.mean_atoms.positions)
-    # plt.xlabel('Principal component')
-    # plt.ylabel('Cumulative variance')
-    # plt.show()
-
-    backbone = universe.select_atoms(atom_select)
+    backbone = universe_1.select_atoms(atom_select)
+    backbone2 = universe_2.select_atoms(atom_select)
+    backbone3 = universe_3.select_atoms(atom_select)
+    backbone4 = universe_4.select_atoms(atom_select)
+    backbone5 = universe_5.select_atoms(atom_select)
+    backbone6 = universe_6.select_atoms(atom_select)
+   
     transformed = pc.transform(backbone, n_components=5)
+    transformed2 = pc.transform(backbone2, n_components=5)
+    transformed3 = pc.transform(backbone3, n_components=5)
+    transformed4 = pc.transform(backbone4, n_components=5)
+    transformed5 = pc.transform(backbone5, n_components=5)
+    transformed6 = pc.transform(backbone6, n_components=5)
+
     pc_df = pd.DataFrame(transformed,
                   columns=['PC{}'.format(i+1) for i in range(5)])
-    pc_df['Time (ps)'] = pc_df.index * universe.trajectory.dt
+    pc_df2 = pd.DataFrame(transformed2,
+                  columns=['PC{}'.format(i+1) for i in range(5)])
+    pc_df3 = pd.DataFrame(transformed3,
+                  columns=['PC{}'.format(i+1) for i in range(5)])
+
+    pc_df4 = pd.DataFrame(transformed4,
+                  columns=['PC{}'.format(i+1) for i in range(5)])
+
+    pc_df5 = pd.DataFrame(transformed5,
+                  columns=['PC{}'.format(i+1) for i in range(5)])
+    pc_df6 = pd.DataFrame(transformed6,
+                  columns=['PC{}'.format(i+1) for i in range(5)])      
+
+    pc_df['Time (ps)'] = pc_df.index * universe_1.trajectory.dt
+    pc_df2['Time (ps)'] = pc_df2.index * universe_2.trajectory.dt
+    pc_df3['Time (ps)'] = pc_df3.index * universe_3.trajectory.dt
+    pc_df4['Time (ps)'] = pc_df4.index * universe_4.trajectory.dt
+    pc_df5['Time (ps)'] = pc_df5.index * universe_5.trajectory.dt
+    pc_df6['Time (ps)'] = pc_df6.index * universe_6.trajectory.dt
+
+    pc.df.toexcel("1.xlsx")
+    pc.df2.toexcel("2.xlsx")
+    pc.df3.toexcel("3.xlsx")
+    pc.df4.toexcel("4.xlsx")
+    pc.df5.toexcel("5.xlsx")
+    pc.df6.toexcel("6.xlsx")
 
     print(pc_df.head())
-    sns.pairplot(pc_df, hue='Time (ps)')
+    sns.pairplot(pc_df, color="grey")
+    sns.pairplot(pc_df2, color="red")
+    sns.pairplot(pc_df3, color="orange")
+    sns.pairplot(pc_df4, color="green")
+    sns.pairplot(pc_df5, color="blue")
+    sns.pairplot(pc_df6, color="purple")
+
     plt.savefig("pairgrid_normal2.png")
     # sns.map(plt.scatter, marker='.')
     plt.show()
